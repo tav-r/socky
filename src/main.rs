46243df1d4034644b3d20c0c51252db3aa3f496b
David Herrmann <david.herrmann@protonmail.com>
2023-05-06 18:19:40 +0200

    first working version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use async_recursion::async_recursion;
+use itertools::{concat, Itertools};
 use nom::{
     branch::alt,
     bytes::complete::{tag, take},
@@ -7,18 +8,18 @@ use nom::{
     number::Endianness,
     sequence::tuple,
 };
-use std::error::Error;
+use std::env::args;
 use std::net::{Ipv4Addr, Ipv6Addr};
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use tokio::io::{copy, split, AsyncWriteExt};
-use tokio::select;
 use tokio::{
     io::AsyncReadExt,
     net::{TcpListener, TcpStream},
+    select,
 };
 
-type AnswerAndTransform =
-    fn(Message) -> Result<(Vec<u8>, State, Option<TcpStream>), Box<dyn Error>>;
+type ResponseAndTransform =
+    Box<dyn Fn(Message) -> Result<(Vec<u8>, State, Option<String>), std::io::Error>>;
 type MessageParser = fn(&[u8]) -> Result<(&[u8], Message), nom::Err<nom::error::Error<&[u8]>>>;
 
 #[derive(PartialEq, Debug)]
@@ -31,31 +32,26 @@ enum State {
 
 #[derive(Debug)]
 struct ClientGreeting {
-    Ver: u8,
-    Auth: Vec<u8>,
+    auth: Vec<u8>,
 }
 
 #[derive(Debug)]
 struct ClientAuthRequest {
-    Ver: u8,
-    Id: Vec<u8>,
-    Pw: Vec<u8>,
+    id: String,
+    pw: String,
 }
 
 #[derive(Debug)]
 struct ClientConnectionRequest {
-    Ver: u8,
-    Cmd: u8,
-    Rsv: u8,
-    Socks5Addr: Sock5AddressType,
-    DstPort: u16,
+    socks5_addr: Sock5AddressType,
+    dstport: u16,
 }
 
 #[derive(Debug)]
 enum Message {
-    ClientGreeting(ClientGreeting),
-    ClientAuthRequest(ClientAuthRequest),
-    ClientConnectionRequest(ClientConnectionRequest),
+    Greeting(ClientGreeting),
+    AuthRequest(ClientAuthRequest),
+    ConnectionRequest(ClientConnectionRequest),
 }
 
 #[derive(Debug)]
@@ -65,39 +61,62 @@ enum Sock5AddressType {
     IPv6(Ipv6Addr),
 }
 
+impl From<&Sock5AddressType> for String {
+    fn from(val: &Sock5AddressType) -> Self {
+        match val {
+            Sock5AddressType::IPv4(ipv4) => ipv4.to_string(),
+            Sock5AddressType::Domain(s) => s.clone(),
+            Sock5AddressType::IPv6(ipv6) => ipv6.to_string(),
+        }
+    }
+}
+
+// Get the message parser for the current state
 fn get_message_parser(state: &State) -> MessageParser {
     match state {
+        // Expect Greeting Message, it consists of a version byte, the number of supported auth methods and the auth methods
         State::Init => |input| {
             map(
-                tuple((nomu8, flat_map(nomu8, take))),
-                |(ver, auth): (u8, &[u8])| {
-                    Message::ClientGreeting(ClientGreeting {
-                        Ver: ver,
-                        Auth: auth.to_vec(),
+                tuple((
+                    tag(&[5u8]),           // version, must be 0x05
+                    flat_map(nomu8, take), // read the specified number of auth methods
+                )),
+                // transform the parsed data into a ClientGreeting struct wrapped in a Message enum
+                |(_, auth): (&[u8], &[u8])| {
+                    Message::Greeting(ClientGreeting {
+                        auth: auth.to_vec(),
                     })
                 },
             )(input)
         },
+        // Expect Auth Request, it consists of a version byte, the username and the password
         State::Authenticating => |input| {
             map(
-                tuple((nomu8, flat_map(nomu8, take), flat_map(nomu8, take))),
-                |(ver, id, pw): (_, &[u8], &[u8])| {
-                    Message::ClientAuthRequest(ClientAuthRequest {
-                        Ver: ver,
-                        Id: id.to_vec(),
-                        Pw: pw.to_vec(),
+                tuple((
+                    tag(&[1u8]),           // auth version, only username/password is supported
+                    flat_map(nomu8, take), // read the specified number of username bytes
+                    flat_map(nomu8, take), // read the specified number of password bytes
+                )),
+                // transform the parsed data into a ClientAuthRequest struct wrapped in a Message enum
+                |(_, id, pw): (_, &[u8], &[u8])| {
+                    Message::AuthRequest(ClientAuthRequest {
+                        id: String::from_utf8(id.to_vec())
+                            .unwrap_or_else(|_| panic!("could not parse username")),
+                        pw: String::from_utf8(pw.to_vec())
+                            .unwrap_or_else(|_| panic!("could not parse password")),
                     })
                 },
             )(input)
         },
+        // Expect Connection Request, it consists of a version byte, a command byte, a reserved byte, the address type and the destination port
         State::Authenticated => |input| {
+            // define parsers for the different address types
             let ipv4_parser = map(tuple((tag(&[1u8]), take(4u8))), |(_, ipv4): (_, &[u8])| {
                 Sock5AddressType::IPv4(
                     Ipv4Addr::try_from(*std::convert::TryInto::<&[u8; 4]>::try_into(ipv4).unwrap())
                         .unwrap_or_else(|_| panic!("Could not parse IPv4 address: {:?}", ipv4)),
                 )
             });
-
             let domain_parser = map(
                 tuple((tag(&[3u8]), flat_map(nomu8, take))),
                 |(_, domain): (_, &[u8])| {
@@ -107,7 +126,6 @@ fn get_message_parser(state: &State) -> MessageParser {
                     )
                 },
             );
-
             let ipv6_parser = map(tuple((tag(&[4u8]), take(16u8))), |(_, ipv6): (_, &[u8])| {
                 Sock5AddressType::IPv6(
                     Ipv6Addr::try_from(
@@ -119,51 +137,122 @@ fn get_message_parser(state: &State) -> MessageParser {
 
             map(
                 tuple((
-                    nomu8,
-                    nomu8,
-                    nomu8,
+                    tag(&[5u8]), // SOCKS5 version
+                    tag(&[1u8]), // command, currently TCP Port Binding and UDP are not supported
+                    tag(&[0u8]), // RSV must be 0x00
                     alt((ipv4_parser, domain_parser, ipv6_parser)),
                     nomu16(Endianness::Big),
                 )),
-                |(ver, cmd, rsv, dstaddr, dstport): (u8, u8, u8, Sock5AddressType, u16)| {
-                    Message::ClientConnectionRequest(ClientConnectionRequest {
-                        Ver: ver,
-                        Cmd: cmd,
-                        Rsv: rsv,
-                        Socks5Addr: dstaddr,
-                        DstPort: dstport,
+                // transform the parsed data into a ClientConnectionRequest struct wrapped in a Message enum
+                |(_, _, _, dstaddr, dstport): (_, _, _, Sock5AddressType, u16)| {
+                    Message::ConnectionRequest(ClientConnectionRequest {
+                        socks5_addr: dstaddr,
+                        dstport,
                     })
                 },
             )(input)
         },
-        State::Connected => panic!("this should not happen!"),
+        State::Connected => panic!("tried to parse message in connected state"),
     }
 }
 
-fn get_answer_and_transform(state: &State) -> AnswerAndTransform {
+// Get a function that returns the response to the given message, the next state and optionally a connection string
+fn get_reponse_and_transform(state: &State, config: Arc<Config>) -> ResponseAndTransform {
     match state {
-        State::Init => todo!(),
-        State::Authenticating => todo!(),
-        State::Authenticated => todo!(),
-        State::Connected => todo!(),
+        // Return "password authentication" response if the client supports it, otherwise return "no acceptable authentication methods
+        State::Init => Box::new(|message| match message {
+            Message::Greeting(m) => {
+                let ver = 5;
+                let cauth = if m.auth.contains(&2) { 2 } else { 0xff };
+
+                Ok((vec![ver, cauth], State::Authenticating, None))
+            }
+            _ => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "invalid message or unsupported authentication method",
+            )),
+        }),
+        // Return "authentication successful" response if the username and password are correct, otherwise return "authentication failed"
+        State::Authenticating => Box::new(move |message| match message {
+            Message::AuthRequest(m) => {
+                let ver = 1;
+                let status = if m.id.eq(&config.username) && m.pw.eq(&config.password) {
+                    0
+                } else {
+                    println!("Authentication failed");
+                    0xff
+                };
+
+                Ok((vec![ver, status], State::Authenticated, None))
+            }
+            _ => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "invalid message or unsupported authentication method",
+            )),
+        }),
+        // Return "connection established" response and the connection string
+        State::Authenticated => Box::new(|message| match message {
+            Message::ConnectionRequest(m) => {
+                let target = format!(
+                    "{}:{}",
+                    std::convert::Into::<String>::into(&m.socks5_addr),
+                    m.dstport
+                );
+
+                Ok((
+                    vec![5, 0, 0]
+                        .into_iter()
+                        .chain(concat(match m.socks5_addr {
+                            Sock5AddressType::IPv4(ipv4) => [vec![1], ipv4.octets().to_vec()],
+                            Sock5AddressType::Domain(domain) => {
+                                [vec![3], domain.as_bytes().to_vec()]
+                            }
+                            Sock5AddressType::IPv6(ipv6) => [vec![4], ipv6.octets().to_vec()],
+                        }))
+                        .chain(m.dstport.to_be_bytes().to_vec())
+                        .collect::<Vec<u8>>(),
+                    State::Connected,
+                    Some(target),
+                ))
+            }
+            _ => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "invalid message",
+            )),
+        }),
+        State::Connected => panic!("tried to get response in connected state"),
     }
 }
 
 async fn process(
     state: State,
     mut stream: TcpStream,
-    mut buf: Vec<u8>,
+    buf: &mut [u8],
+    config: Arc<Config>,
 ) -> Result<(State, TcpStream, Option<TcpStream>), Box<dyn std::error::Error>> {
-    buf.fill(0);
+    // read from stream
+    let n = stream.read(buf).await?;
 
-    // read message from stream
-    let _ = stream.read_buf(&mut buf).await?;
-    let (_, message) = get_message_parser(&state)(&buf)
-        .unwrap_or_else(|_| panic!("parse error in state {:?} for data {:?}", state, buf));
+    // parse message
+    let (_, message) = get_message_parser(&state)(&buf[..n]).map_err(|e| {
+        std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            format!("parse error in state {:?} for data {:?}: {}", state, buf, e),
+        )
+    })?;
 
-    // write answer to stream
-    let (answer, next_state, remote_stream) = get_answer_and_transform(&state)(message)?;
-    stream.write_all(&answer).await?;
+    // get response and next state
+    let (response, next_state, conn_string) = get_reponse_and_transform(&state, config)(message)?;
+
+    // connect to remote if necessary
+    let remote_stream = if let Some(conn_string) = conn_string {
+        Some(TcpStream::connect(conn_string).await?)
+    } else {
+        None
+    };
+
+    // write response to stream
+    stream.write_all(&response).await?;
 
     // return next state and stream
     Ok((next_state, stream, remote_stream))
@@ -173,27 +262,62 @@ async fn process(
 async fn process_protocol(
     state: State,
     client_stream: TcpStream,
+    config: Arc<Config>,
 ) -> Result<(State, TcpStream, TcpStream), Box<dyn std::error::Error>> {
-    let buf = vec![0; 1024];
-    let (next_state, client_stream, remote_stream) = process(state, client_stream, buf).await?;
+    let mut buf = [0u8; 1024];
+    let (next_state, client_stream, remote_stream) =
+        process(state, client_stream, &mut buf, config.clone()).await?;
 
-    if next_state == State::Connected {
-        return Ok((next_state, client_stream, remote_stream.unwrap()));
+    match next_state {
+        State::Connected => Ok((
+            next_state,
+            client_stream,
+            remote_stream
+                .ok_or("reached 'Connected' state but remote connection was not established")?,
+        )),
+        _ => process_protocol(next_state, client_stream, config).await,
     }
+}
 
-    process_protocol(next_state, client_stream).await
+#[derive(Debug)]
+struct Config {
+    pub username: String,
+    pub password: String,
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+    let mut arguments = args().skip(1);
+
+    let bind_string = arguments.next().ok_or("No bind string provided")?;
+    let creds_arg = arguments
+        .next()
+        .ok_or("No username and password provided")?;
+
+    let (username, password) = creds_arg
+        .splitn(2, ':')
+        .collect_tuple()
+        .ok_or("Invalid username:password string")?;
+
+    let config = Arc::new(Config {
+        username: username.to_string(),
+        password: password.to_string(),
+    });
+
+    let listener = TcpListener::bind(&bind_string).await?;
+
     loop {
         let (stream, _addr) = listener.accept().await?;
 
+        let config = config.clone();
+
         tokio::spawn(async move {
-            let (_, client_stream, remote_stream) = process_protocol(State::Init, stream)
+            let (_, client_stream, remote_stream) = process_protocol(State::Init, stream, config)
                 .await
-                .expect("protocol error");
+                .unwrap_or_else(|e| {
+                    eprintln!("error while processing protocol: {}", e);
+                    panic!()
+                });
 
             // forward packets from client to target server and vice versa if connection was established
             let (mut cread, mut cwrite) = split(client_stream);
